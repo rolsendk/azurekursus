79bafaf370922f3b2decf9276b9bed61fad713b5
Doktor Olsen <roldit@outlook.com>
2019-12-09 14:19:46 +0100

    Teknologisk.ActiveDirectory web app project with Azure AD controlled login

---
+++ Teknologisk.ActiveDirectory/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Teknologisk.ActiveDirectory.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
